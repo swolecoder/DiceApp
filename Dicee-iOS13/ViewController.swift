c2c780fb4233cd8b7270f3b2cd36c17567ba9737
Ashish Ranjan <aranjan@mbp-aranjan.local>
2019-11-05 09:33:46 -0500

    adding the UI

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //imageLIteral
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFive");
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo");
+    }
+
+
+}
+
